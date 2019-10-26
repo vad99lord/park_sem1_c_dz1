68782aef3ddbaa40832219d17700c640741bbc8f
vad99lord <vad99lord@gmail.com>
2019-10-26 10:59:23 +0300

    add more build targets and test in yaml.

--- TestSimpleSeq.cpp
+++ TestSimpleSeq.cpp
@@ -10,7 +10,7 @@ extern "C" {
 TEST(SeqCount, ValidityTest) {
 
     size_t size = 0;
-    char *buffer = readFile("../smallDuplicate.txt", &size);
+    char *buffer = readFile("smallDuplicate.txt", &size);
     Sequence seq = findCommonSeq(buffer, size);
     std::cout << "Testing file with one long sequence.....\n";
     ASSERT_TRUE(seq.elem=='j');
@@ -18,14 +18,14 @@ TEST(SeqCount, ValidityTest) {
     deleteBuf(buffer);
 
     size = 0;
-    buffer = readFile("../samplesmall.txt", &size);
+    buffer = readFile("samplesmall.txt", &size);
     seq = findCommonSeq(buffer, size);
     std::cout << "Testing small string with no duplicates....\n";
     ASSERT_TRUE(seq.length==2||seq.length==3);
     deleteBuf(buffer);
 
     size = 0;
-    buffer = readFile("../sample.txt", &size);
+    buffer = readFile("sample.txt", &size);
     seq = findCommonSeq(buffer, size);
     std::cout << "Testing medium sample with many small sequences....\n";
     printf("\n");
